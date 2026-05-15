b7455a83aadc78e21e35a4852011da6b71d86dd3
Kotadiya Dhruv <108579548+Dhruvkotadiya90@users.noreply.github.com>
2026-05-15 10:17:06 +0530

    Commit

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,44 +1,21 @@
-const js = require('@eslint/js');
-const globals = require('globals');
-const tseslint = require('typescript-eslint');
-const reactHooks = require('eslint-plugin-react-hooks');
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-module.exports = tseslint.config(
+export default defineConfig([
+  globalIgnores(['dist']),
   {
-    ignores: [
-      'build',
-      'dist',
-      'coverage',
-      'node_modules',
-      'public',
-      'eslint.config.js',
-      'babel.config.js',
-      'config-overrides.js',
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
     ],
-  },
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
+      globals: globals.browser,
+      parserOptions: { ecmaFeatures: { jsx: true } },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    },
-  }
-);
+  },
+])
