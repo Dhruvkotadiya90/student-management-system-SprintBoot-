9cf0e1209b76b95767d7b558d134d2d81881a1e4
Kotadiya Dhruv <108579548+Dhruvkotadiya90@users.noreply.github.com>
2026-05-30 13:09:54 +0530

    Updates

--- frontend/src/services/StudentService.js
+++ frontend/src/services/StudentService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "/api/students";
+const API_URL = "http://localhost:8081//students"; // {/api}
 
 // ✅ GET all students
 export const getStudents = () => {
