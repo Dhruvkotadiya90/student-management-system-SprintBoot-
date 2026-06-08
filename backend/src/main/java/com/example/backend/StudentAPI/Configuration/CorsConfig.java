be17f8c73370b076e464c002c7db4686e779d770
Kotadiya Dhruv <108579548+Dhruvkotadiya90@users.noreply.github.com>
2026-06-08 17:49:23 +0530

    Updates

--- backend/src/main/java/com/example/backend/StudentAPI/Configuration/CorsConfig.java
+++ backend/src/main/java/com/example/backend/StudentAPI/Configuration/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://dhruvsm.netlify.app/")
+                        .allowedOrigins("https://dhruvsm.netlify.app")
                         .allowedMethods("*")
                         .allowCredentials(true);
             }
