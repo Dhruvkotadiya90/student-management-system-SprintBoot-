cb9a8fb1982e283aa204b6f0caacea249eec8d6c
Kotadiya Dhruv <108579548+Dhruvkotadiya90@users.noreply.github.com>
2026-06-08 15:57:17 +0530

    Updates

--- frontend/src/services/StudentService.js
+++ frontend/src/services/StudentService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8081/students"; // {/api}
+const API_URL = import.meta.env.VITE_API_URL; // {/api}
 
 // ✅ GET all students
 export const getStudents = () => {
