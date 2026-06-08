1b6034a0d37f70dd482766394296ae157aa3791b
Kotadiya Dhruv <108579548+Dhruvkotadiya90@users.noreply.github.com>
2026-06-08 16:12:26 +0530

    Updates

--- backend/src/main/java/com/example/backend/StudentAPI/controller/StudentController.java
+++ backend/src/main/java/com/example/backend/StudentAPI/controller/StudentController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.backend.StudentAPI.model.Student;
 import com.example.backend.StudentAPI.service.IStudentService;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://dhruvsm.netlify.app/")
 
 @RestController
 @RequestMapping("/students")
