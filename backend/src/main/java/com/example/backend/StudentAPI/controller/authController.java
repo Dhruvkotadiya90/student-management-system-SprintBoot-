245a16359714bff1ca5d36ee07c72ffeb4d244ff
Kotadiya Dhruv <108579548+Dhruvkotadiya90@users.noreply.github.com>
2026-06-08 17:07:07 +0530

    Updates

--- backend/src/main/java/com/example/backend/StudentAPI/controller/authController.java
+++ backend/src/main/java/com/example/backend/StudentAPI/controller/authController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Value;
 
 @RestController
-@RequestMapping("/auth/login")
+@RequestMapping("/auth")
 @CrossOrigin(origins = "https://dhruvsm.netlify.app/", allowCredentials = "true")
 public class authController {
 
