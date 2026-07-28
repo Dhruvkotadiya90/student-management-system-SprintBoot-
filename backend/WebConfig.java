b24ecf5102b366ed12951145692d112c582903f1
Kotadiya Dhruv <108579548+Dhruvkotadiya90@users.noreply.github.com>
2026-07-28 12:11:16 +0530

    Updates

--- backend/WebConfig.java
+++ backend/WebConfig.java
@@ -4,7 +4,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://dhruvsm.onrender.com/students/students")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
