bb684769c7c0d446ba90738dd62032bddf5b7db2
Kotadiya Dhruv <108579548+Dhruvkotadiya90@users.noreply.github.com>
2026-07-28 12:02:54 +0530

    Updates

--- frontend/src/services/StudentService.js
+++ frontend/src/services/StudentService.js
@@ -2,28 +2,17 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-// helper to get token
-const getAuthHeader = () => {
-  const token = localStorage.getItem("token");
-
-  return {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-};
-
 // GET all students
 export const getStudents = () => {
-  return axios.get(`${API_URL}/students`, getAuthHeader());
+  return axios.get(`${API_URL}/students`);
 };
 
 // ADD student
 export const addStudent = (student) => {
-  return axios.post(`${API_URL}/students`, student, getAuthHeader());
+  return axios.post(`${API_URL}/students`, student);
 };
 
 // DELETE student
 export const deleteStudent = (id) => {
-  return axios.delete(`${API_URL}/students/${id}`, getAuthHeader());
+  return axios.delete(`${API_URL}/students/${id}`);
 };
